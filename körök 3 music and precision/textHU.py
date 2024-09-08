a26608625f082146fcdce79cdf98a6fbecefcf4a
Rinatin <130601569+Siminmindav@users.noreply.github.com>
2024-09-08 18:27:31 +0200

    Add files via upload

---
+++ körök 3 music and precision/textHU.py
@@ -0,0 +1,29 @@
+caption = "Végtelen spirál"
+fps = f"Képkockák másodpercenként: "
+
+instructions = [
+    "---Irányítások---",
+    "Szóköz:                      Új négyzetet hoz létre.",
+    "Bal egérgomb:             Elhúzza a középső négyzetet.",
+    "Középső egérgomb:     Vált változók között.",
+    "Görgő fel:                    Az adott változót növeli.",
+    "Görgő le:                     Az adott változót csökkenti.",
+    "Jobb egérgomb:          Törli az előzőleg létre hozott négyzetet.",
+    "OK"
+    ]
+
+variables = [
+    "Sebesség: ",
+    " fok/képkocka",
+    "Méret: ",
+    " px^2",
+    "Sugár: ",
+    " px",
+    "Piros: ",
+    " színérték",
+    "Zöld: ",
+    " színérték",
+    "Kék: ",
+    " színérték",
+    "Semmi"
+]
