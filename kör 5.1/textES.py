3a27b886637f182755fa2ebc0c0ad868797f84bd
Siminmindev <130601569+Siminmindav@users.noreply.github.com>
2025-11-17 18:04:54 +0100

    Add files via upload

---
+++ kör 5.1/textES.py
@@ -0,0 +1,42 @@
+caption = "Espiral infinita"
+fps = f"Fotogramas por segundo: "
+
+instructions = [
+    "---Controles---",
+    "Espacio:                                       Crea un nuevo cuadrado.",
+    "Botón izquierdo del ratón:             Arrastra el cuadrado del medio.",
+    "Botón central del ratón:                Cambia la variable modificable.",
+    "Rueda del ratón hacia arriba:        Aumenta la variable seleccionada.",
+    "Rueda del ratón hacia abajo:         Disminuye la variable seleccionada.",
+    "Botón derecho del ratón:               Elimina el último cuadrado invocado.",
+    "OK"
+    ]
+
+variables = [
+    "Velocidad: ",
+    " grado/por fotograma",
+    "Tamaño: ",
+    " px^2",
+    "Radio: ",
+    " px",
+    "Rojo: ",
+    " valor de color",
+    "Verde: ",
+    " valor de color",
+    "Azul: ",
+    " valor de color",
+    "Nada"
+]
+
+developer = [
+    "Creador: Siminmin",
+    "",
+    "Música: Stall C418",
+    "",
+    "github link:",
+    "    https://github.com/Siminmindav/spinner",
+    "",
+    "",
+    "Gracias por jugar!",
+    "",
+]
